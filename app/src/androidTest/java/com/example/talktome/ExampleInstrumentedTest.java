d7acdb23640f3395c8be6ff07d6b7c58d5d1919a
Yannick Gruber <yannick.gruber@fhwn.ac.at>
2020-06-15 17:41:50 +0200

    Added Test Function CallContactByName, new activities for settings and adding of caregivers, general styling

--- app/src/androidTest/java/com/example/talktome/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/talktome/ExampleInstrumentedTest.java
@@ -1,25 +1,87 @@
 package com.example.talktome;
 
+import android.Manifest;
+import android.app.Instrumentation;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.provider.ContactsContract;
+import android.util.Log;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission.READ_CONTACTS, Manifest.permission.CALL_PHONE);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.talktome", appContext.getPackageName());
+    }
+
+    @Test
+    public void CallContactByName() {
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        Context appContext = instrumentation.getTargetContext();
+        List<ContactModel> contacts = new ArrayList<>();
+        ContentResolver contentResolver = appContext.getContentResolver();
+        Cursor cursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " LIKE " + "'%NAME HERE%'",
+                null,
+                null);
+
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                if (cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor cursorInfo = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id},
+                            null
+                    );
+
+                    if (cursorInfo.moveToNext()) {
+                        ContactModel contact = new ContactModel();
+                        contact.id = id;
+                        contact.name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        contact.mobileNumber = cursorInfo.getString(cursorInfo.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contacts.add(contact);
+                    }
+                    cursorInfo.close();
+                }
+            }
+            cursor.close();
+        }
+
+        for (ContactModel contact:
+             contacts) {
+            Log.d("Contact", contact.id + " - " + contact.name + " - " + contact.mobileNumber);
+        }
+
+        if (!contacts.isEmpty()) {
+            Intent i = new Intent(Intent.ACTION_CALL);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.setData(Uri.parse("tel:" + contacts.get(0).mobileNumber));
+            appContext.startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/talktome/WorkingSpace.java
+++ app/src/main/java/com/example/talktome/WorkingSpace.java
@@ -1,7 +1,10 @@
 package com.example.talktome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class WorkingSpace extends AppCompatActivity {
 
@@ -11,4 +14,8 @@ public class WorkingSpace extends AppCompatActivity {
         setContentView(R.layout.activity_working_space);
     }
 
+    public void gotoSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
 }
