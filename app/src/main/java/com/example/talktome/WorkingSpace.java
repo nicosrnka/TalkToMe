d09e6273171f70e3585d63b18d09f5e860967c08
Oliver Dragschitz <oliver@MacBook-Pro.local>
2020-06-17 13:11:07 +0200

    Merge branch 'development' of /Users/oliver/StudioProjects/TalkToMe with conflicts.

--- app/src/main/java/com/example/talktome/WorkingSpace.java
+++ app/src/main/java/com/example/talktome/WorkingSpace.java
@@ -1,10 +1,7 @@
 package com.example.talktome;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 public class WorkingSpace extends AppCompatActivity {
 
@@ -14,8 +11,4 @@ public class WorkingSpace extends AppCompatActivity {
         setContentView(R.layout.activity_working_space);
     }
 
-    public void gotoSettings(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
 }
