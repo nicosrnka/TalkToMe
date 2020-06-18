4510950c2885204a9af8075194f2e4458713549b
oliver <oliver.dragschitz@gmail.com>
2020-06-18 10:36:10 +0200

    voice input supplement

--- app/src/main/java/com/example/talktome/WorkingSpace.java
+++ app/src/main/java/com/example/talktome/WorkingSpace.java
@@ -1,19 +1,58 @@
 package com.example.talktome;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import voice_control.SpeechInputHandler;
 
 public class WorkingSpace extends AppCompatActivity {
+
+    private SpeechInputHandler speechInputHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_working_space);
+        this.speechInputHandler = new SpeechInputHandler();
     }
 
     public void gotoSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void getSpeechInput(View view)
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(intent, 1);
+        }
+        else
+        {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && data != null && requestCode == 1)
+        {
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            this.speechInputHandler.handle(result.get(0));
+        }
     }
 }
